0e6b40b9bb1c7209c98d84df3b7339c1aacedf97
Bunga Mungil <bungamungil@icloud.com>
2026-09-14 05:02:51 +0700

    feat(verticaltabs): add flat VerticalTabsWidget panel
    Add VerticalTabsWidget, a QWidget panel with a toolbar (collapse +
    search buttons), a pinned TabListView, a flat normal TabListView, and
    a bottom new-tab button, built on the Task 1 TabListView/TabFilterModel
    classes. Nothing consumes it yet (Task 4).
    
    Also fixes the src/lib/CMakeLists.txt directory ordering: the
    verticaltabs/* entries were inserted before tools/* out of alphabetical
    order in a prior commit; moved them after tools/* (before webengine/*)
    and added the new files alphabetically within the verticaltabs block.

---
+++ src/lib/verticaltabs/verticaltabswidget.cpp
@@ -0,0 +1,201 @@
+/* ============================================================
+* VerticalTabs plugin for Falkon
+* Copyright (C) 2018 David Rosca <nowrep@gmail.com>
+* Copyright (C) 2026 Bunga Mungil <bungamungil@icloud.com>
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+* ============================================================ */
+#include "verticaltabswidget.h"
+#include "tablistview.h"
+#include "tabfiltermodel.h"
+
+#include "webtab.h"
+#include "tabmodel.h"
+#include "tabwidget.h"
+#include "toolbutton.h"
+#include "browserwindow.h"
+#include "iconprovider.h"
+#include "qzsettings.h"
+
+#include <QIcon>
+#include <QUrl>
+#include <QScrollBar>
+#include <QHBoxLayout>
+#include <QVBoxLayout>
+#include <QWheelEvent>
+
+VerticalTabsWidget::VerticalTabsWidget(BrowserWindow *window, QWidget *parent)
+    : QWidget(parent)
+    , m_window(window)
+{
+    auto *layout = new QVBoxLayout(this);
+    layout->setSpacing(0);
+    layout->setContentsMargins(0, 0, 0, 0);
+
+    auto *toolBarLayout = new QHBoxLayout();
+
+    auto *collapseButton = new ToolButton(this);
+    collapseButton->setObjectName(QSL("verticaltabs-button-collapse"));
+    collapseButton->setAutoRaise(true);
+    collapseButton->setFocusPolicy(Qt::NoFocus);
+    collapseButton->setToolTip(tr("Collapse Sidebar"));
+    collapseButton->setIcon(QIcon::fromTheme(QSL("sidebar-collapse")));
+    m_collapseButton = collapseButton;
+
+    auto *searchButton = new ToolButton(this);
+    searchButton->setObjectName(QSL("verticaltabs-button-search"));
+    searchButton->setAutoRaise(true);
+    searchButton->setFocusPolicy(Qt::NoFocus);
+    searchButton->setToolTip(tr("Search Tabs"));
+    QIcon searchIcon = QIcon::fromTheme(QSL("search"));
+    if (searchIcon.isNull()) {
+        searchIcon = QIcon(QSL(":/icons/menu/search-icon.svg"));
+    }
+    searchButton->setIcon(searchIcon);
+    m_searchButton = searchButton;
+    connect(searchButton, &QAbstractButton::clicked, this, &VerticalTabsWidget::searchRequested);
+
+    toolBarLayout->addWidget(collapseButton);
+    toolBarLayout->addStretch();
+    toolBarLayout->addWidget(searchButton);
+
+    m_pinnedView = new TabListView(m_window, this);
+    auto *pinnedModel = new TabFilterModel(m_pinnedView);
+    pinnedModel->setFilterPinnedTabs(false);
+    pinnedModel->setRejectDropOnLastIndex(true);
+    pinnedModel->setSourceModel(m_window->tabModel());
+    m_pinnedView->setModel(pinnedModel);
+    m_pinnedView->setHideWhenEmpty(true);
+
+    m_normalView = new TabListView(m_window, this);
+    auto *normalModel = new TabFilterModel(m_normalView);
+    normalModel->setFilterPinnedTabs(true);
+    normalModel->setSourceModel(m_window->tabModel());
+    m_normalView->setModel(normalModel);
+    m_pinnedView->setFocusProxy(m_normalView);
+
+    auto *newTabButton = new ToolButton(this);
+    newTabButton->setObjectName(QSL("verticaltabs-button-addtab"));
+    newTabButton->setAutoRaise(true);
+    newTabButton->setFocusPolicy(Qt::NoFocus);
+    newTabButton->setToolTip(tr("New Tab"));
+    newTabButton->setIcon(IconProvider::newTabIcon());
+    newTabButton->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Preferred);
+    connect(newTabButton, &QAbstractButton::clicked, this, [this]() {
+        m_window->tabWidget()->addView(QUrl(), Qz::NT_SelectedNewEmptyTab);
+    });
+    m_newTabButton = newTabButton;
+
+    layout->addLayout(toolBarLayout);
+    layout->addWidget(m_pinnedView);
+    layout->addWidget(m_normalView, 1);
+    layout->addWidget(newTabButton);
+}
+
+void VerticalTabsWidget::setIconOnly(bool enable)
+{
+    m_iconOnly = enable;
+}
+
+bool VerticalTabsWidget::isIconOnly() const
+{
+    return m_iconOnly;
+}
+
+QAbstractButton *VerticalTabsWidget::searchButton() const
+{
+    return m_searchButton;
+}
+
+void VerticalTabsWidget::switchToNextTab()
+{
+    WebTab *tab = nextTab();
+    if (tab) {
+        tab->makeCurrentTab();
+    }
+}
+
+void VerticalTabsWidget::switchToPreviousTab()
+{
+    WebTab *tab = previousTab();
+    if (tab) {
+        tab->makeCurrentTab();
+    }
+}
+
+WebTab *VerticalTabsWidget::nextTab() const
+{
+    QModelIndex next;
+    if (m_window->tabWidget()->webTab()->isPinned()) {
+        next = m_pinnedView->indexAfter(m_pinnedView->currentIndex());
+        if (!next.isValid()) {
+            next = m_normalView->model()->index(0, 0);
+        }
+    } else {
+        next = m_normalView->indexAfter(m_normalView->currentIndex());
+        if (!next.isValid()) {
+            next = m_pinnedView->model()->index(0, 0);
+        }
+    }
+    return next.data(TabModel::WebTabRole).value<WebTab*>();
+}
+
+WebTab *VerticalTabsWidget::previousTab() const
+{
+    QModelIndex previous;
+    if (m_window->tabWidget()->webTab()->isPinned()) {
+        previous = m_pinnedView->indexBefore(m_pinnedView->currentIndex());
+        if (!previous.isValid()) {
+            previous = m_normalView->model()->index(m_normalView->model()->rowCount() - 1, 0);
+        }
+    } else {
+        previous = m_normalView->indexBefore(m_normalView->currentIndex());
+        if (!previous.isValid()) {
+            previous = m_pinnedView->model()->index(m_pinnedView->model()->rowCount() - 1, 0);
+        }
+    }
+    return previous.data(TabModel::WebTabRole).value<WebTab*>();
+}
+
+void VerticalTabsWidget::wheelEvent(QWheelEvent *event)
+{
+    if (!qzSettings->alwaysSwitchTabsWithWheel && m_normalView->verticalScrollBar()->isVisible()) {
+        return;
+    }
+
+    if (   m_normalView->verticalScrollBar()->isVisible()
+        && m_normalView->verticalScrollBar()->rect().contains(event->position().toPoint())
+    ) {
+        return;
+    }
+
+    m_wheelHelper.processEvent(event);
+    while (WheelHelper::Direction direction = m_wheelHelper.takeDirection()) {
+        switch (direction) {
+        case WheelHelper::WheelUp:
+        case WheelHelper::WheelLeft:
+            switchToPreviousTab();
+            break;
+
+        case WheelHelper::WheelDown:
+        case WheelHelper::WheelRight:
+            switchToNextTab();
+            break;
+
+        default:
+            break;
+        }
+    }
+    event->accept();
+}
---
+++ src/lib/verticaltabs/verticaltabswidget.h
@@ -0,0 +1,65 @@
+/* ============================================================
+* VerticalTabs plugin for Falkon
+* Copyright (C) 2018 David Rosca <nowrep@gmail.com>
+* Copyright (C) 2026 Bunga Mungil <bungamungil@icloud.com>
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+* ============================================================ */
+#pragma once
+
+#include "qzcommon.h"
+#include "wheelhelper.h"
+
+#include <QWidget>
+
+class BrowserWindow;
+class TabListView;
+class WebTab;
+
+class QAbstractButton;
+
+class FALKON_EXPORT VerticalTabsWidget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit VerticalTabsWidget(BrowserWindow *window, QWidget *parent = nullptr);
+
+    void setIconOnly(bool enable);
+    bool isIconOnly() const;
+
+    QAbstractButton *searchButton() const;
+
+    void switchToNextTab();
+    void switchToPreviousTab();
+
+Q_SIGNALS:
+    void searchRequested();
+
+protected:
+    void wheelEvent(QWheelEvent *event) override;
+
+private:
+    WebTab *nextTab() const;
+    WebTab *previousTab() const;
+
+    BrowserWindow *m_window;
+    TabListView *m_pinnedView;
+    TabListView *m_normalView;
+    QAbstractButton *m_collapseButton;
+    QAbstractButton *m_searchButton;
+    QAbstractButton *m_newTabButton;
+    WheelHelper m_wheelHelper;
+    bool m_iconOnly = false;
+};
